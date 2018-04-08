044aa68f8ff0848b9b355045c85a18e744947c40
zhouzechen <188338648782@163.com>
2018-04-08 20:18:50 +0800

    add view type

--- app/src/main/java/com/zzc/layoutattrhelper/ThemeAdapter.java
+++ app/src/main/java/com/zzc/layoutattrhelper/ThemeAdapter.java
@@ -6,7 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.zzc.layoutattrhelper.databinding.ItemThemeBinding;
+import com.zzc.layoutattrhelper.databinding.ItemThemeABinding;
+import com.zzc.layoutattrhelper.databinding.ItemThemeBBinding;
 
 /**
  * auth zzc
@@ -15,16 +16,31 @@ import com.zzc.layoutattrhelper.databinding.ItemThemeBinding;
  */
 
 public class ThemeAdapter extends RecyclerView.Adapter<ThemeAdapter.ThemeHolder> {
+    private static final int TYPE_ONE = 1;
+    private static final int TYPE_TWO = 2;
 
     @Override
     public ThemeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_theme, parent, false);
-        return new ThemeHolder(inflate);
+        LayoutInflater from = LayoutInflater.from(parent.getContext());
+        switch (viewType) {
+            case TYPE_ONE:
+                View inflate = from.inflate(R.layout.item_theme_a, parent, false);
+                return new ThemeAHolder(inflate);
+            case TYPE_TWO:
+            default:
+                View inflateB = from.inflate(R.layout.item_theme_b, parent, false);
+                return new ThemeBHolder(inflateB);
+        }
     }
 
     @Override
     public void onBindViewHolder(ThemeHolder holder, int position) {
-        holder.mBinding.text.setText("item " + position);
+        holder.setText("item " + position);
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position > 10 && position % 2 == 0 ? TYPE_ONE : TYPE_TWO;
     }
 
     @Override
@@ -32,13 +48,42 @@ public class ThemeAdapter extends RecyclerView.Adapter<ThemeAdapter.ThemeHolder>
         return 100;
     }
 
-    public static class ThemeHolder extends RecyclerView.ViewHolder {
-
-        private final ItemThemeBinding mBinding;
+    public abstract static class ThemeHolder extends RecyclerView.ViewHolder {
 
         public ThemeHolder(View itemView) {
             super(itemView);
+        }
+
+        public abstract void setText(String text);
+    }
+
+    public static class ThemeAHolder extends ThemeHolder {
+
+        private final ItemThemeABinding mBinding;
+
+        public ThemeAHolder(View itemView) {
+            super(itemView);
             mBinding = DataBindingUtil.bind(itemView);
+        }
+
+        @Override
+        public void setText(String text) {
+            mBinding.text.setText(text);
+        }
+    }
+
+    public static class ThemeBHolder extends ThemeHolder {
+
+        private final ItemThemeBBinding mBinding;
+
+        public ThemeBHolder(View itemView) {
+            super(itemView);
+            mBinding = DataBindingUtil.bind(itemView);
+        }
+
+        @Override
+        public void setText(String text) {
+            mBinding.text.setText(text);
         }
     }
 }
